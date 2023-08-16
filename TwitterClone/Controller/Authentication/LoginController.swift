06933645795d2671ffa9bbf517df41b0e187aa70
bahittinn <bahittin84@gmail.com>
2023-08-16 20:27:54 +0300

    login controller

---
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginController.swift
+//  TwitterClone
+//
+//  Created by Bahittin on 16.08.2023.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    //MARK: - Properties
+    
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    
+    
+    //MARK: - helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+    
+}
