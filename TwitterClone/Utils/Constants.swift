4c837bac755ae8ab8cb5627e8f13b6c7078db03c
bahittinn <bahittin84@gmail.com>
2023-08-18 12:05:50 +0300

    upload firebase to user image

---
+++ TwitterClone/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TwitterClone
+//
+//  Created by Bahittin on 18.08.2023.
+//
+
+import Firebase
+import FirebaseStorage
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
