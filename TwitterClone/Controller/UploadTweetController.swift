da6480c0f99aea82258d984961eba343ca541faa
bahittinn <bahittin84@gmail.com>
2023-08-21 23:17:43 +0300

    improvements for ui

--- TwitterClone/Controller/UploadTweetController.swift
+++ TwitterClone/Controller/UploadTweetController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import SDWebImage
 
 class UploadTweetController: UIViewController {
     
     //MARK: - Properties
+    
+    private let user: User
     
     private lazy var actionButton: UIButton = {
         let button = UIButton(type: .system)
@@ -27,7 +30,27 @@ class UploadTweetController: UIViewController {
         return button
     }()
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
     //MARK: - Life Cycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -35,7 +58,7 @@ class UploadTweetController: UIViewController {
     
     //MARK: - Selectors
     
-    @objc func handleCancel() {
+    @objc func handleCancel()  {
         dismiss(animated: true)
     }
     
@@ -44,15 +67,26 @@ class UploadTweetController: UIViewController {
     }
     //MARK: - API
     
+    
+    
     //MARK: - Helpers
     
     func configureUI() {
         view.backgroundColor = .white
+        configureNavigationBar()
+        configureImageView()
+    }
+    
+    func configureNavigationBar() {
         navigationController?.navigationBar.tintColor = .twitterBlue
-        
-        
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
+    }
+    
+    func configureImageView() {
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 16)
+        guard let url = URL(string: user.profileImageUrl) else { return }
+        profileImageView.sd_setImage(with: url)
     }
 }
