d23fab4e4435bd0a0694ee66250e24b1b657f41a
bahittinn <bahittin84@gmail.com>
2023-08-22 21:33:02 +0300

    Tweet model

---
+++ TwitterClone/Model/Tweet.swift
@@ -0,0 +1,30 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by Bahittin on 22.08.2023.
+//
+
+import Foundation
+
+struct Tweet {
+    let caption: String
+    let tweetID: String
+    let likes: Int
+    var timestamp: Date!
+    let uid: String
+    let retweetCount: Int
+    
+    init(tweetID: String, dictionary: [String: AnyObject]) {
+        self.tweetID = tweetID
+        
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+        self.retweetCount = dictionary["retweets"] as? Int ?? 0
+        self.uid = dictionary["caption"] as? String ?? ""
+        
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+    }
+}
--- TwitterClone/View/CaptionTextView.swift
+++ TwitterClone/View/CaptionTextView.swift
@@ -5,6 +5,7 @@
 //  Created by Bahittin on 21.08.2023.
 //
 
+
 import UIKit
 
 class CaptionTextView: UITextView {
