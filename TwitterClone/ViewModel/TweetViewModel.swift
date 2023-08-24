a90039ccffffe9a1646a790262fabfec40a72f89
bahittinn <bahittin84@gmail.com>
2023-08-24 20:31:35 +0300

    first viewmodel

---
+++ TwitterClone/ViewModel/TweetViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TweetViewModel.swift
+//  TwitterClone
+//
+//  Created by Bahittin on 24.08.2023.
+//
+
+import Foundation
+
+struct TweetViewModel {
+    let tweet: Tweet
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+    }
+}
