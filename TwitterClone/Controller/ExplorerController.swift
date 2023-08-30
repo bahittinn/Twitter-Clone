b6261d6efc1975d480ee0b6575fdaae6552468fe
bahittinn <bahittin84@gmail.com>
2023-08-31 00:21:46 +0300

    complete ui and fetch users for explore

--- TwitterClone/Controller/ExplorerController.swift
+++ TwitterClone/Controller/ExplorerController.swift
@@ -13,12 +13,25 @@ class ExplorerController: UITableViewController {
 
     //MARK: - Properties
     
+    private var users = [User]() {
+        didSet { tableView.reloadData() }
+    }
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        fetchUsers()
+    }
+    
+    //MARK: - API
+    
+    func fetchUsers() {
+        UserService.shared.fetchUsers { users in
+                self.users = users
+        }
     }
     
     //MARK: - Helpers
@@ -29,12 +42,13 @@ class ExplorerController: UITableViewController {
         
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 60
+        tableView.separatorStyle = .none
     }
 }
 
 extension ExplorerController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- TwitterClone/View/UserCell.swift
+++ TwitterClone/View/UserCell.swift
@@ -11,13 +11,43 @@ class UserCell: UITableViewCell {
     
     //MARK: - Properties
     
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
     
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
     //MARK: - Lifecycle
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = .systemPurple
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
     }
     
     required init?(coder: NSCoder) {
